26a25c866556a3811f886fd11e3c3c4306cd5307
Diana-Gaytan <49350217+Diana-Gaytan@users.noreply.github.com>
2019-11-29 15:25:09 -0600

    Add files via upload

--- ProgramaAguacates.py
+++ ProgramaAguacates.py
@@ -1,4 +1,10 @@
 import csv
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.random import rand
+import csv
+import math 
+#from math import *
 Lat1= float(input("Introduzca latitud 1 en x: "))
 Lon1= float(input("Introduzca longuitud 1 en y: "))
 Lat2= float(input("Introduzca latitud 2 en x: "))
@@ -6,14 +12,15 @@ Lon2= float(input("Introduzca longuitud 2 en y: "))
 #DY = Lat2-Lat1
 #DY = Lon2-Lon1
 #variables de para la georeferencias
+x1=[]
+y1=[]
 Y = Lat1
 X = Lon1
-IncY = 1
-IncX = 1
+IncY = 0.34124547
+IncX = 0.24315497
 #Variables paras el CSV
 ff=open("PuntosAGUACATES.csv","w")
 salida=csv.writer(ff)
-
 print("Coordenadas para plantar arbolitos")
 while Y < Lat2:
     Y = (Y + IncY)
@@ -21,13 +28,26 @@ while Y < Lat2:
         X=(X +IncX)
         coordx=X
         coordy=Y
+        x1.append(coordx)
+        y1.append(coordy)
         print(coordx,",",coordy)
         salida.writerow((coordx,coordy))
     X = Lon1
     print("-------------------------")
 del salida
 ff.close()
-    
+
+print (x1)
+print (y1)
+
+
+plt.plot(x1,y1, 'o', Label='Aguacates')
+plt.xlabel('x1')
+plt.ylabel('y1')
+plt.title('Aguacates Cliente')
+plt.legend()
+plt.show()
+
 #file="PuntosAGUACATE.csv"
 #csv=open(file,"w")
 #PuntosAGUACATE= [
